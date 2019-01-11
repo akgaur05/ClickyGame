2e9e35762f13be0a695df722fcfca821d1d60233
Akanksha Kapoor <agaur05@gmail.com>
2019-01-10 20:19:32 -0600

    Updates

---
+++ precache-manifest.8f902ecd787601eb26bed9ed287f708e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "006e065bf3629050c75f",
+    "url": "/ClickyGame/static/css/main.11a57620.chunk.css"
+  },
+  {
+    "revision": "006e065bf3629050c75f",
+    "url": "/ClickyGame/static/js/main.006e065b.chunk.js"
+  },
+  {
+    "revision": "790a7416686c195da3c6",
+    "url": "/ClickyGame/static/js/1.790a7416.chunk.js"
+  },
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "406761132d04355cdfa0f93d61608808",
+    "url": "/ClickyGame/index.html"
+  }
+];
